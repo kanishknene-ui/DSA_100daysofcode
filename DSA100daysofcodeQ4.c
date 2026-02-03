593f9f4de84f8f6df36b22655d8d4d850c0093c4
kanishknene-ui <kanishk.nene@gmail.com>
2026-02-03 14:29:16 +0530

    Add files via upload

---
+++ DSA100daysofcodeQ4.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+    int value;
+    int comparison;
+    int n=5;
+    int a[]={10,20,30,40,50};
+    printf("enter the value of the array: ",value);
+    scanf("%d",&value);
+for(int i=0;i<n;i++){
+    if(a[i]==value){
+        printf("the index of the array %d\n",i);
+comparison=i+1;
+    printf("the comparison is %d\n",comparison);
+    }
+    
+}
+return 0;
+}
