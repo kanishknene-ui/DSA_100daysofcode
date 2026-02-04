e67a3d87ce993850ee2b97eaebf790b43d765d3e
kanishknene-ui <kanishk.nene@gmail.com>
2026-02-04 11:12:42 +0530

    Add files via upload

---
+++ DSA100daysofcodeQ7.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int n=5;
+    int arr[]={1,2,3,4,5};
+for(int i=0;i<n;i++){
+printf("%d",arr[i]);
+}
+printf("\n");
+for(int i=4;i>=0;i--){
+    printf("%d",arr[i]);
+}
+return 0;
+}
